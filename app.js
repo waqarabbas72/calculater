d1298160db55ba15ad3a5bbf77c1dc47cfbc8f09
waqarabbas72 <sheraliate751@gmail.com>
2022-09-06 15:36:06 +0500

    new

---
+++ app.js
@@ -0,0 +1,22 @@
+// for buttons
+const buttons = document.querySelectorAll ('button');
+// for display
+const display = document.querySelector('.display');
+buttons.forEach(function(button){
+    button.addEventListener('click',(calculate))
+})
+
+function calculate(e){
+    const clickvalue = e.target.value;
+    console.log(clickvalue);
+   if (clickvalue === '=') {
+    if(clickvalue !== '')
+    display.value = eval(display.value)
+     }
+     else if (clickvalue === 'C')
+     {
+        display.value = ''
+     }else(
+        display.value += clickvalue
+     )
+}
